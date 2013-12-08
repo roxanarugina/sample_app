05c13943d80f49894868e57b43cd6df25d7164b8
roxana.rugina <roxana.rugina@gmail.com>
2013-12-08 20:49:01 +0000

    add :size for gravatar_for helper and error messages tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -161,4 +161,12 @@ describe "signup" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
+
+  #   describe "after saving the user" do
+  #       before { click_button submit }
+  #       let(:user) { User.find_by(email: 'user@example.com') }
+
+  #       it { should have_title(user.name) }
+  #       it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+  #     end
   end
