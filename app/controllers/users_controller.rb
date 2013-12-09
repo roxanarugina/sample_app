3158db6efc22a4f4266ce7530536de5889ef047a
roxana.rugina <roxana.rugina@gmail.com>
2013-12-09 02:06:58 +0000

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,16 @@ class UsersController < ApplicationController
 
   	def create
     	@user = User.new(user_params)
-    	if @user.save
-    		flash[:success] = "Welcome to the Sample App!"
-      	 redirect_to @user 
-    	else
-    	  render 'new'
-    	end
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   	end
 
-  	private
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
