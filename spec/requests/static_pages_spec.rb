0f84a713a90a411d8aa107abf2c7a87dfc3e36ee
roxana.rugina <roxana.rugina@gmail.com>
2013-12-13 15:05:13 +0000

    modif spec user model and app user controller

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,16 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-  subject {page}
+  subject { page }
 
   describe "Home page" do
-  before { visit root_path }
+    before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title('') }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
-  
 
   describe "Help page" do
     before { visit help_path }
